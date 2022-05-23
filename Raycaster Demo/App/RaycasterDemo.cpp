3bd3ca0a9c9801566077c4dfceb1d5b79cbba40c
Catalin Popa <61079773+Catalin142@users.noreply.github.com>
2022-05-23 18:07:04 +0300

    updare layout

--- Raycaster Demo/App/RaycasterDemo.cpp
+++ Raycaster Demo/App/RaycasterDemo.cpp
@@ -3,16 +3,17 @@
 void RaycasterDemo::onAttach()
 {
 	m_Frame = std::make_shared<Sprite>("Resources/Frame.spr");
+
 	m_TextBox = std::make_shared<TextBox>(m_Frame, 160.0f, 20.0f);
 	m_TextBox->setPosition(Center | Bottom);
 	m_TextBox->setOffset(30, 10);
 	m_TextBox->setText("Minotaur Labyrinth", Center, { 1.0f, 1.0f, 1.0f });
 
 	m_Scene = std::make_shared<Scene>();
-	m_Scene->loadScene("Resources/labyrinth.scene");
+	m_Scene->loadScene("Resources/dungeon.scene");
 
 	m_Camera = std::make_shared<Camera>(60.0f, 3.0f);
-	WorldRenderer::setGlobalIlluminationIntensity(0.7f);
+	WorldRenderer::setGlobalIlluminationIntensity(0.9f);
 
 	Input::showCursor(false);
 }
